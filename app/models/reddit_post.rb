3a8b9d0f3dd43f53bc86771e491ef2772b498775
Konrad Ulman <veradopl@gmail.com>
2021-02-01 21:01:10 +0100

    Update RedditService

---
+++ app/models/reddit_post.rb
@@ -0,0 +1,2 @@
+class RedditPost < ApplicationRecord
+end
--- app/services/RedditService.rb
+++ app/services/RedditService.rb
@@ -1,15 +1,61 @@
 require 'httparty'
+require 'ostruct'
 
 class RedditService
   @logger = Logger.new($stdout)
 
   OAUTH_URL = 'https://www.reddit.com/api/v1/access_token'.freeze
+  WALL_STREET_URL = 'https://oauth.reddit.com/r/wallstreetbets/hot?'.freeze
   @token = nil
+
+  def self.posts(limit = 25, after = nil)
+    request_url = WALL_STREET_URL + "&limit=#{limit}"
+    request_url += "&after=#{after}" unless after.nil?
+
+    response = HTTParty.get(request_url, headers: {
+      'Authorization': "Bearer #{token}",
+      'User-Agent': 'RedditStocks/0.1 by Ulmii'
+    })
+
+    parsed_data = JSON.parse(response.body)['data']['children']
+
+    parsed_data.collect do |obj|
+      parsed_post = obj['data']
+      OpenStruct.new({ name: parsed_post['name'], title: parsed_post['title'], selftext: parsed_post['selftext'] })
+    end
+  end
 
   def self.token
     @token = generate_token if @token == nil
 
     @token
+  end
+
+  def self.runner
+    posts_loaded = FALSE
+    after = nil
+    limit = 25
+    while posts_loaded == FALSE
+      posts = self.posts(limit, after)
+      after = posts[-1].name
+      posts_loaded = save_posts(posts)
+      limit = 100 unless posts_loaded
+    end
+  end
+
+  private_class_method def self.save_posts(posts)
+    old_batch = TRUE
+    posts.each do |post|
+      if RedditPost.where(name: post.name).blank?
+        post = RedditPost.create(name: post.name, title: post.title, selftext: post.selftext)
+        post.save
+        old_batch = FALSE
+      else
+        old_batch = TRUE
+      end
+    end
+
+    old_batch
   end
 
   private_class_method def self.generate_token
