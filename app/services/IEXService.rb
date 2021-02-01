58e4e21a2fe81c82eeb1a5cb3384bae667944447
Konrad Ulman <veradopl@gmail.com>
2021-02-01 21:27:34 +0100

    Add IEX url

--- app/services/IEXService.rb
+++ app/services/IEXService.rb
@@ -2,7 +2,7 @@ require 'httparty'
 
 class IEXService
   @logger = Logger.new($stdout)
-  OAUTH_URL = 'https://cloud.iexapis.com/beta/ref-data/symbols?token='.freeze
+  IEX_URL = 'https://cloud.iexapis.com/beta/ref-data/symbols?token='.freeze
   @stocks = nil
 
   def self.stocks(limit = 25, offset = 0)
@@ -12,7 +12,7 @@ class IEXService
   def self.load_stocks
     @logger.info('Loading stocks list')
     iex_token = ENV['IEX_TOKEN']
-    stocks_url = "https://cloud.iexapis.com/beta/ref-data/symbols?token=#{iex_token}"
+    stocks_url = IEX_URL + iex_token
     response = HTTParty.get(stocks_url)
     data = JSON.parse(response.body)
     data.collect do |obj|
